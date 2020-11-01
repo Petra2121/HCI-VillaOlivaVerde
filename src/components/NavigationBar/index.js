01e582c23768fface2cb4e7a54b0d74e3645efb5
Petra2121 <petra21lovric@gmail.com>
2020-11-01 14:14:41 +0100

    Final touches

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -4,13 +4,13 @@ import { navs as navTabs } from '../../constants/const'
 import styles from './style.module.css'
 
 const NavigationBar = ({ activeTab, useThisStyle }) => (
-  <nav className={useThisStyle ? styles.useThisStyle  : styles.navigationBar}>
+  <nav className={styles[useThisStyle || 'navigationBar']}>
       {navTabs.map(tab => <li className={tab === activeTab ? styles.active : ''}>
           {tab}</li>
       )}
   </nav>
 )
 
-//nav className={styles[useThisStyle || navigationBar]}
+//nav className={useThisStyle ? styles.useThisStyle  : styles.navigationBar}
 
 export default NavigationBar
