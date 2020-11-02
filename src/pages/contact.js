d5a105e6b414bfbd86b86783b299f173c0b8c1e2
Petra2121 <petra21lovric@gmail.com>
2020-11-02 10:13:00 +0100

    contact page init

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+// src/pages/contact.js
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+ 
+const ContactPage = () => (
+    <HeaderFooterLayout activeTab="Contact">
+    </HeaderFooterLayout>
+)
+ 
+export default ContactPage
